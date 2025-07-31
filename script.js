ed34af3fb02ffbd496d63f1132220934bead5890
Gizy Michel <batblover@yahoo.com>
2025-07-30 20:00:03 -0400

    initialized git repository

--- script.js
+++ script.js
@@ -5,26 +5,14 @@ let APIKey = "826b9a2e";
 let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
-const movies = [
-    {
-     title: "Guardians of the Galaxy Vol. 2", 
-     rating: "7.6/10",
-     posterUrl: "https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg"
-    },
-    {
-     title: "The Matrix", 
-     rating: "8.7/10",
-     posterUrl: "https://image.tmdb.org/t/p/original/p96dm7sCMn4VYAStA6siNz30G1r.jpg"
-    },
-    {title: "Interstellar", 
-     rating: "8.7/10",
-     posterUrl: "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg" 
-    },
-    {title: "The Shawshank Redemption", 
-     rating: "9.3/10",
-     posterUrl: "https://image.tmdb.org/t/p/original/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg"
-    },
-];
+async function main() {
+    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=826b9a2e");
+    const moviesData = await movies.json();
+    `<div class="movie
+    </div>`
+}
+
+main();
 
 function displayMovies() {
     const movieContainer = document.getElementById('movieContainer');
@@ -65,10 +53,3 @@ if (filterMovies.length > 0) {
 }
 
 document.getElementById('searchBtn').addEventListener('click', searchMovie);
-
-console.log("Sorted by Title A to Z:", movies.sort((a,b) => a.title.localeCompare(b.title)));
-console.log("Sorted by Title Z to A:", movies.sort((a, b) => b.title.localeCompare(a.title)));
-console.log("Sorted by Rating (Lowest to Highest):", movies.sort((a, b) => a.rating - b.rating));
-console.log("Sorted by Rating (Highest to Lowest):", movies.sort((a, b) => b.rating - a.rating));
-console.log("Sorted by Year (Oldest to Newest):", movies.sort((a, b) => a.year - b.year));
-console.log("Sorted by Year (Newest to Oldest):", movies.sort((a, b) => b.year - a.year));
